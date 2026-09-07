277c571db139605fd3ca82d38ab97631b7444178
KshitijBinoj01 <kshitij.binoj2025@vitstudent.ac.in>
2026-09-07 12:51:59 +0530

    Create entry point for mobile portrait app

---
+++ demo1/frontend/proofpass-wallet/src/main-mobile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import MobilePortraitApp from "./MobilePortraitApp";
+
+createRoot(document.getElementById("root")).render(<MobilePortraitApp />);
